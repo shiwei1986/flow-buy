f6648894233305f1f331f7b264a0f662166a567f
admin <admin@wushiwei>
2020-07-29 17:03:27 +0800

    12

--- src/main/java/com/senld/gzlt/flowBuy/utils/OrderUtils.java
+++ src/main/java/com/senld/gzlt/flowBuy/utils/OrderUtils.java
@@ -25,12 +25,12 @@ public class OrderUtils {
 	}
 
 	public static void main(String[] args) {
-//		for (int i = 0; i < 20; i++) {
-//			System.out.println(getOrderNo("L"));
-//		}
-//		System.out.println("L201911282031013253".substring(1, 15));
+		// for (int i = 0; i < 20; i++) {
+		// System.out.println(getOrderNo("L"));
+		// }
+		// System.out.println("L201911282031013253".substring(1, 15));
 		Double aDouble = 39.00;
-		int i = (int) (aDouble/5);
+		int i = (int) (aDouble / 5);
 		System.out.println(i);
 		System.out.println(getOrderNo("O"));
 	}
